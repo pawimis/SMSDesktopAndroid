af2e1b3df670ebe2a6244ba5d3a21b159b286854
pawimis <pawimis@gmail.com>
2016-08-20 13:07:04 +0200

    Update VARS.java

--- app/src/main/java/com/example/pmisi/desktopsms/VARS.java
+++ app/src/main/java/com/example/pmisi/desktopsms/VARS.java
@@ -4,6 +4,6 @@ package com.example.pmisi.desktopsms;
  * Created by pmisi on 12.08.2016.
  */
 public class VARS {
-    public static final String SERVERAPIKEY = "AIzaSyDyMSd1WhP1RKxJsnZekTCHum_WricxL2c";
-    public static final String SENDERID = "823079405908";
+    public static final String SERVERAPIKEY = "xxxxx";
+    public static final String SENDERID = "xxxx";
 }
